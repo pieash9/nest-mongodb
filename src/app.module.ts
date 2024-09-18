725c9729ffe81ade8deaf9eb2318f64038ef4a35
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-09-18 07:27:47 +0600

    update db

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,13 @@ import { SongsModule } from './songs/songs.module';
 import { AlbumsModule } from './albums/albums.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/spotify-clone'), SongsModule, AlbumsModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://pieash9:nVoDIXXg0nKuhJMX@cluster0.2au1tbt.mongodb.net/spotify-clone?retryWrites=true&w=majority',
+    ),
+    SongsModule,
+    AlbumsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
